737b0f87d213d66a686226769f3dc699e0d5ce6b
SENNQQ <topor4ik02@gmail.com>
2022-02-15 20:35:03 +0200

    models patients

--- patients/models.py
+++ patients/models.py
@@ -1,3 +1,21 @@
 from django.db import models
 
-# Create your models here.
+
+class Patients(models.Model):
+    Sex = models.CharField(max_length=20, choices=(('Женский', 'Женский'), ('Мужской', 'Мужской')))
+    Name = models.CharField(max_length=100)
+    Surname = models.CharField(max_length=100)
+    Patronymic = models.CharField(max_length=100)
+    Date_of_birth = models.DateField(blank=True)
+    Place_of_residence = models.CharField(max_length=100)
+    Blood_type = models.CharField(max_length=10, choices=(('I+', 'I+'), ('II+', 'II+'), ('III+', 'III+'),
+                                                         ('IV+', 'IV+'), ('I-', 'I-'), ('II-', 'II-'), ('III-',
+                                                                                                        'III-'),
+                                                         ('IV-', 'IV-')))
+    Telephone = models.CharField(max_length=100)
+    Email = models.CharField(max_length=30)
+    # Medical_card = models.OneToOneField(on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.Name + self.Surname
+

1c445459339088b0d168c13047b0e86d3754f117
SENNQQ <topor4ik02@gmail.com>
2022-02-18 23:06:34 +0200

    based forms profile patient

---
+++ salutem/static/js/PatientProfile.js
@@ -0,0 +1,47 @@
+document.querySelector(".submit__form").addEventListener("click", (e)=>{
+    e.preventDefault();
+    $.ajax({
+        url: '/patients/save_profile/',
+        type: 'POST',
+        dataType: 'json',
+        data: {
+            pk: location.href.split('/')[5],
+            Sex: $('#powermail_field_anrede').val(),
+            username: $('#powermail_field_vorname').val(),
+            surname: $('#powermail_field_nachname').val(),
+            Patronymic: $('#powermail_field_Patronymic').val(),
+            Date_of_birth: $('#powermail_field_geburtsdatum').val(),
+            phone: $('#powermail_field_telefonnummer').val(),
+            email: $('#powermail_field_e_mail').val(),
+            Place_of_residence: $('#powermail_field_residence').val(),
+            Blood_type: $('#powermail_field_blood').val(),
+        },
+        success: (data) => {
+            if('errors' in data){
+                console.log(1);
+            }
+            if('success' in data){
+                console.log(0);
+                // window.location.replace(window.location.origin + data.success)
+            }
+        }
+    })
+})
+
+let SelectSex = document.querySelector("#powermail_field_anrede").options;
+let SelectBlood = document.querySelector("#powermail_field_blood").options;
+
+let hiddenSex = document.querySelector("#hiddenSex").innerText;
+let hiddenBlood = document.querySelector("#hiddenBlood").innerText;
+
+function selectedOption(select, type) {
+    for(let i=0; i<select.length; i++){
+    if(select[i].value===type){
+        select[i].selected = true;
+    }
+}
+
+}
+
+selectedOption(SelectSex, hiddenSex);
+selectedOption(SelectBlood, hiddenBlood);

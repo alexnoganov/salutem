369d097a729742e972382e826f8fd58d1b635e2c
SENNQQ <topor4ik02@gmail.com>
2022-03-12 20:33:18 +0200

    make medicalcard

--- patients/urls.py
+++ patients/urls.py
@@ -1,11 +1,12 @@
 from django.contrib.auth.decorators import login_required
-from django.urls import path
+from django.urls import path, include
 
 from . import views
 
 urlpatterns = [
     path('', login_required(views.PatientsView.as_view()), name='patients'),
     path('profile/<int:pk>/', login_required(views.EditingPatient.as_view()), name='profile'),
+    path('profile/<int:pk>/medicalcard', include('medicalCard.urls')),
     path('save_profile/', views.profile_user, name="save_profile"),
     path('analysis/', views.get_analysis, name="analysis"),
 ]
--- salutem/settings.py
+++ salutem/settings.py
@@ -39,6 +39,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'user',
     'patients',
+    'medicalCard',
     'widget_tweaks',
 ]
 

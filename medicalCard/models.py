2ed299913821f38d756f54e90732d1375304a60f
SENNQQ <topor4ik02@gmail.com>
2022-03-14 21:26:22 +0200

    поработал над выводом мед книги в шаблон, есть вопросы к тебе, надо подумать

--- medicalCard/models.py
+++ medicalCard/models.py
@@ -1,3 +1,13 @@
 from django.db import models
+from patients.models import Patients
 
-# Create your models here.
+
+class MedicalCard(models.Model):
+    patient = models.ForeignKey(Patients, on_delete=models.CASCADE)
+    purpose = models.CharField(max_length=300)
+    symptoms = models.CharField(max_length=300)
+    treatment = models.CharField(max_length=500)
+
+    def __str__(self):
+        return self.patient.Name + self.patient.Surname
+
--- medicalCard/views.py
+++ medicalCard/views.py
@@ -1,19 +1,19 @@
 from django.shortcuts import render
 from django.views.generic import DetailView
 
-from patients.models import Patients
+from medicalCard.models import MedicalCard
+from patients.models import Analyzes
 
 
 class EditingPatient(DetailView):
     template_name = 'medicalCard/medicalCard.html'
 
-    model = Patients
-    context_object_name = 'profile'
+    model = MedicalCard
+    context_object_name = 'medicalCard'
 
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
-        # context['form'] = PatientForm()
-        # context['analyzes'] = AnalyzesType.objects.all().order_by("title")
+        context['analyzes'] = Analyzes.objects.filter(patient_id=self.kwargs['pk'])
         # if self.request.user.has_perm('user.edit_analyzes'):
         #     context['patient_analyzes'] = list(
         #         Analyzes.objects.filter(patient_id=self.kwargs['pk'],
